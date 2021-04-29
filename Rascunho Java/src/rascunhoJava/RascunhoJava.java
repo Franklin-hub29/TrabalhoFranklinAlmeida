b95f06ca364092f22bede3e61782c2da59eab068
Franklin-hub29 <81197526+Franklin-hub29@users.noreply.github.com>
2021-04-29 01:19:01 -0300

    Add files via upload

---
+++ Rascunho Java/src/rascunhoJava/RascunhoJava.java
@@ -0,0 +1,23 @@
+package rascunhoJava;
+
+public class RascunhoJava {
+	
+	public static void main(String[]args) {
+		
+		float unidade = 1330;
+		 float centimetro ;
+		
+	      float  metros =  unidade  / 100;
+	            centimetro = metros / 100;
+	     
+	      
+		
+		
+
+	
+	System.out.println("Resultado "+metros+centimetro);
+	
+
+	}
+
+}
